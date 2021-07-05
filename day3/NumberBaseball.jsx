e17894b6891b9caca40de542f0cffa59dd1f8dab
ByeongJun <neostgeart@ajou.ac.kr>
2021-07-06 02:32:59 +0900

    Numberbaseball

--- day3/NumberBaseball.jsx
+++ day3/NumberBaseball.jsx
@@ -1,46 +1,88 @@
+import React, { Component, createRef, useState } from 'react';
+import Try from './Try';
 
-const React = require('react');
-
-const getNumbers = () => {
-
+function getNumbers() { // 숫자 네 개를 겹치지 않고 랜덤하게 뽑는 함수
+    const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const array = [];
+    for (let i = 0; i < 4; i += 1) {
+        const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+        array.push(chosen);
+    }
+    return array;
 }
 
-class NumberBaseball extends React.Component{
-    
-    state = {
-        result : '',
-        value : '',
-        answer : getNumbers(),
-        tries : [],
-    }
-    
-    onSubmitForm = () => {
+const NumberBaseball = () => {
 
-    }
+    const [ result, setResult ] = useState('');
+    const [ value, setValue ] = useState('');
+    const [ answer, setAnswer ] = useState(getNumbers());
+    const [ tries, setTries ] = useState([]);
+    const inputEl = React.useRef(null);
 
-    onChangeInput = () => {
+    const onSubmitForm = (e) => {
+        e.preventDefault();
+        if (value === answer.join('')) {
+            setResult('홈런');
+            setTries((prevTries)=>{
+                return [...prevTries,{try:value,result:"홈런!"}];
+            })
+            
+            alert('게임을 다시 시작합니다!');
+            setValue('');
+            setAnswer(getNumbers());
+            setTries([]);
+         
+            inputEl.current.focus();
+        } else { // 답 틀렸으면
+            const answerArray = value.split('').map((v) => parseInt(v));
+            let strike = 0;
+            let ball = 0;
+            if (tries.length >= 9) { // 10번 이상 틀렸을 때
+                setResult(`10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다!`);
+                alert('게임을 다시 시작합니다!');
+                setValue('');
+                setAnswer(getNumbers());
+                setTries([]);
+                
+                inputEl.current.focus();
+            } else {
+                for (let i = 0; i < 4; i += 1) {
+                    if (answerArray[i] === answer[i]) {
+                        strike += 1;
+                    } else if (answer.includes(answerArray[i])) {
+                        ball += 1;
+                    }
+                }
+                setTries((prevTries)=>{
+                    return [...prevTries, { try: value, result: `${strike} 스트라이크, ${ball} 볼입니다` }]
+                });
+                setValue('');
+                inputEl.current.focus();
+            }
+        }
+    };
 
-    }
+    const onChangeInput = (e) => {
+        setValue(e.target.value);
+    };
 
-    render(){
-        return(
-            <>
-                <h1>{this.state.result}</h1>
-                <form onSubmit = {this.onSubmitForm}>
-                    <input maxLength = {4} value = {this.state.value} onChange = {this.onChangeInput}/>
-                    <button></button>
-                </form>
-                <div>시도 : {this.state.tries.length}</div>
-                <ul>
-                    {['사과','바나나','포도','귤','감','배'].map((value,index)=>{
-                        return(
-                            <li>{value}{index}</li>
-                        );
-                    })}
-                </ul>
-            </>
-        );
-    }
+    return (
+        <>
+            <h1>{result}</h1>
+            <form onSubmit={onSubmitForm}>
+                <input ref={inputEl} maxLength={4} value={value} onChange={onChangeInput} />
+                <button></button>
+            </form>
+            <div>시도: {tries.length}</div>
+            <ul>
+                {tries.map((v, i) => {
+                    return (
+                        <Try key={`${i + 1}차 시도 :`} tryInfo={v} />
+                    );
+                })}
+            </ul>
+        </>
+    );
 }
 
-export default NumberBaseball;
+export default NumberBaseball; // import NumberBaseball;
